530be10b14d6d2b530acfb36dc5657c111b0cf1a
Kongo Malick <malicktakykongo@gmail.com>
2023-08-21 10:48:46 +0000

    Update the file 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -9,5 +9,17 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+/**
+ * struct op - struct op
+ * @op: operator
+ * @f: the function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
 #endif
