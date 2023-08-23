69c78bec92deb09a5dbc457697e4295ab275f18e
Kongo Malick <malicktakykongo@gmail.com>
2023-08-23 11:13:59 +0000

    Update the file 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d->name != NULL)
 		printf("Name: %s\n", d->name);
 	else
-		printf("(nil)\n");
+		printf("Name: (nil)\n");
 
 	if (d->age != 0)
 		printf("Age: %f\n", d->age);
@@ -21,5 +21,5 @@ void print_dog(struct dog *d)
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
-		printf("(nil)\n");
+		printf("Owner: (nil)\n");
 }
