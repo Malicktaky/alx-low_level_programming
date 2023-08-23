26b02220e4b2560b04b75c183df70a15fdc4a79d
Kongo Malick <malicktakykongo@gmail.com>
2023-08-23 11:22:27 +0000

    Update the readm file and dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,12 @@
  *
  * Description: This is a struct of the dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
