e6a915d5cbba214341ae22c127dc654b1679de7d
Kongo Malick <malicktakykongo@gmail.com>
2023-08-27 12:07:25 +0000

    Update the file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,33 +23,30 @@ int main(int ac, char **argv)
 		printf("0\n");
 		return (0);
 	}
-	while (cents != 0)
+	while (cents >= 25)
 	{
-		while (cents >= 25)
-		{
-			cents = cents - 25;
-			coins++;
-		}
-		while (cents >= 10 && cents < 25)
-		{
-			cents = cents - 10;
-			coins++;
-		}
-		while (cents >= 5 && cents < 10)
-		{
-			cents = cents - 5;
-			coins++;
-		}
-		while (cents >= 2 && cents < 5)
-		{
-			cents = cents - 2;
-			coins++;
-		}
-		if (cents == 1)
-		{
-			cents = cents - 1;
-			coins++;
-		}
+		cents = cents - 25;
+		coins++;
+	}
+	while (cents >= 10 && cents < 25)
+	{
+		cents = cents - 10;
+		coins++;
+	}
+	while (cents >= 5 && cents < 10)
+	{
+		cents = cents - 5;
+		coins++;
+	}
+	while (cents >= 2 && cents < 5)
+	{
+		cents = cents - 2;
+		coins++;
+	}
+	if (cents == 1)
+	{
+		cents -= 1;
+		coins++;
 	}
 	printf("%d\n", coins);
 	return (0);
