d63bf034eade2a440c46841f613de334536eae72
Kongo Malick <malicktakykongo@gmail.com>
2023-08-29 15:25:31 +0000

    Update the file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,6 +31,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = NULL;
 
 
+	if ((*head) == NULL)
+	{
+		(*head) = new_node;
+		return (*head);
+	}
 	tmp = (*head);
 	while (j < idx && tmp)
 	{
