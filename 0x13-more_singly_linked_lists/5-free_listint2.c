14c48ece601b41c46dfce4acd079af519784ab5b
Kongo Malick <malicktakykongo@gmail.com>
2023-08-29 10:28:38 +0000

    Add the file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: the list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while ((*head)->next)
+	{
+		tmp = (*head)->next;
+		free((*head));
+		(*head) = tmp;
+	}
+	free((*head));
+	*head = NULL;
+}
